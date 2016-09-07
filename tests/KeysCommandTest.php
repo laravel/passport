1f20297fc1adc190856793f16c06cddd1dca5455
Sam Greenwood <sam@samgreenwood.me>
2016-09-07 14:22:41 +0930

    Use phpseclib for generating keys

---
+++ tests/KeysCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+function storage_path($file = null)
+{
+    return __DIR__ . DIRECTORY_SEPARATOR . $file;
+}
+
+class KeysCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+
+        @unlink(storage_path('oauth-private.key'));
+        @unlink(storage_path('oauth-public.key'));
+    }
+
+    public function testPrivateAndPublicKeysAreGenerated()
+    {
+        $command = Mockery::mock(Laravel\Passport\Console\KeysCommand::class)
+            ->makePartial()
+            ->shouldReceive('info')
+            ->with('Encryption keys generated successfully.')
+            ->getMock();
+
+        $rsa = new phpseclib\Crypt\RSA();
+
+        $command->handle($rsa);
+
+        $this->assertFileExists(storage_path('oauth-private.key'));
+        $this->assertFileExists(storage_path('oauth-public.key'));
+    }
+}
