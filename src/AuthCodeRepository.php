46d397b5c8e774be25389a563173fa2bf289924e
Kevin Dierkx <contact@kevindierkx.com>
2016-09-25 20:41:10 +0200

    Replaced auth codes database connection with repository
    Replaced refresh token database connection with repository

---
+++ src/AuthCodeRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Laravel\Passport;
+
+use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
+
+class AuthCodeRepository
+{
+    /**
+     * Get a auth code by the given ID.
+     *
+     * @param  int  $id
+     * @return AuthCode|null
+     */
+    public function find($id)
+    {
+        return AuthCode::find($id);
+    }
+
+    /**
+     * Store a new auth code.
+     *
+     * @param  array  $attributes
+     * @return AuthCode
+     */
+    public function create(array $attributes)
+    {
+        $authCode = (new AuthCode)->forceFill($attributes);
+
+        $authCode->save();
+
+        return $authCode;
+    }
+
+    /**
+     * Revoke an auth code.
+     *
+     * @param  string  $id
+     * @return bool|int
+     */
+    public function revoke($id)
+    {
+        return $this->find($id)->update(['revoked' => true]);
+    }
+
+    /**
+     * Determine if the given auth code is revoked.
+     *
+     * @param  int  $id
+     * @return bool
+     */
+    public function revoked($id)
+    {
+        return AuthCode::where('id', $id)->where('revoked', true)->exists();
+    }
+}
---
+++ src/RefreshTokenRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Laravel\Passport;
+
+class RefreshTokenRepository
+{
+    /**
+     * Get a token by the given ID.
+     *
+     * @param  string  $id
+     * @return RefreshToken
+     */
+    public function find($id)
+    {
+        return RefreshToken::find($id);
+    }
+
+    /**
+     * Creates a new Access Token
+     *
+     * @param  array  $attributes
+     * @return RefreshToken
+     */
+    public function create(array $attributes)
+    {
+        $authCode = (new RefreshToken)->forceFill($attributes)
+            ->save();
+
+        return $authCode;
+    }
+
+    /**
+     * Revoke a refresh token.
+     *
+     * @param  string  $id
+     * @return bool|int
+     */
+    public function revoke($id)
+    {
+        return $this->find($id)->update(['revoked' => true]);
+    }
+
+    /**
+     * Check if the given refresh token has been revoked.
+     *
+     * @param  string  $id
+     * @return bool
+     */
+    public function revoked($id)
+    {
+        return RefreshToken::where('id', $id)->where('revoked', 1)->exists();
+    }
+}
