28220582d7e930114df5aa47f0299da3d16373b5
Kevin Dierkx <contact@kevindierkx.com>
2016-09-26 11:42:12 +0200

    Added support for custom primary keys on user model, resolves: #106

--- src/Http/Controllers/ClientController.php
+++ src/Http/Controllers/ClientController.php
@@ -2,11 +2,12 @@
 
 namespace Laravel\Passport\Http\Controllers;
 
-use Laravel\Passport\Client;
+use Illuminate\Contracts\Validation\Factory as ValidationFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Laravel\Passport\Client;
 use Laravel\Passport\ClientRepository;
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 
 class ClientController
 {
@@ -46,7 +47,9 @@ class ClientController
      */
     public function forUser(Request $request)
     {
-        return $this->clients->activeForUser($request->user()->id)->makeVisible('secret');
+        $userId = $request->user() instanceof Model ? $request->user()->getKey() : $request->user()->id;
+
+        return $this->clients->activeForUser($userId)->makeVisible('secret');
     }
 
     /**
@@ -62,8 +65,10 @@ class ClientController
             'redirect' => 'required|url',
         ])->validate();
 
+        $userId = $request->user() instanceof Model ? $request->user()->getKey() : $request->user()->id;
+
         return $this->clients->create(
-            $request->user()->id, $request->name, $request->redirect
+            $userId, $request->name, $request->redirect
         )->makeVisible('secret');
     }
 
