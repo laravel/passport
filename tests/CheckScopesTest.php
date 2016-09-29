160ccfaa28e77cb85c2f9ac47ec879998d0a9b63
shaun <shaun.keating@biteda.com>
2016-09-29 09:39:50 +0100

    Updated CheckScopesTest to check for AuthorizationException not AuthenticationException.

--- tests/CheckScopesTest.php
+++ tests/CheckScopesTest.php
@@ -26,7 +26,7 @@ class CheckScopesTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException Illuminate\Auth\AuthenticationException
+     * @expectedException \Illuminate\Auth\Access\AuthorizationException
      */
     public function test_exception_is_thrown_if_token_doesnt_have_scope()
     {
