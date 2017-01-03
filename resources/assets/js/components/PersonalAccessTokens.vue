cd8b6070f81896b305fd4907806e05563522792c
Mohamed Said <themohamedsaid@gmail.com>
2017-01-03 16:19:52 +0200

    Switch to using axios instead of vue-resource

--- resources/assets/js/components/PersonalAccessTokens.vue
+++ resources/assets/js/components/PersonalAccessTokens.vue
@@ -209,7 +209,7 @@
              * Get all of the personal access tokens for the user.
              */
             getTokens() {
-                this.$http.get('/oauth/personal-access-tokens')
+                axios.get('/oauth/personal-access-tokens')
                         .then(response => {
                             this.tokens = response.data;
                         });
@@ -219,7 +219,7 @@
              * Get all of the available scopes.
              */
             getScopes() {
-                this.$http.get('/oauth/scopes')
+                axios.get('/oauth/scopes')
                         .then(response => {
                             this.scopes = response.data;
                         });
@@ -240,7 +240,7 @@
 
                 this.form.errors = [];
 
-                this.$http.post('/oauth/personal-access-tokens', this.form)
+                axios.post('/oauth/personal-access-tokens', this.form)
                         .then(response => {
                             this.form.name = '';
                             this.form.scopes = [];
@@ -292,7 +292,7 @@
              * Revoke the given token.
              */
             revoke(token) {
-                this.$http.delete('/oauth/personal-access-tokens/' + token.id)
+                axios.delete('/oauth/personal-access-tokens/' + token.id)
                         .then(response => {
                             this.getTokens();
                         });
