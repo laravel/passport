71e7769dff0c597fd32cf979123fac1212e5aa5d
Amr Fayad <amr.fci2007@gmail.com>
2018-06-19 11:36:31 +0200

    Private Key Length
    Remove private key length from config

--- config/passport.php
+++ config/passport.php
@@ -16,7 +16,5 @@ return [
     'private_key' => env('PASSPORT_PRIVATE_KEY'),
 
     'public_key' => env('PASSPORT_PUBLIC_KEY'),
-    
-    'private_key_length' => env('PASSPORT_PRIVATE_KEY_LENGTH') ? (int)env('PASSPORT_PRIVATE_KEY_LENGTH') : 4096  , 
 
 ];
