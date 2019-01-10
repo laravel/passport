7047eabfb76d788a06e98d58be71eded11ab3a2f
Dries Vints <dries.vints@gmail.com>
2019-01-10 16:45:53 +0100

    Fix revoked refresh token check
    When a check if performed to determine if a refresh token is revoked or not, it's unnecessary to also check if its corresponding access token is revoked. It's perfectly possible to issue a new access token if the old access token is either revoked or pruned as long as the refresh token is valid.
    
    Fixes https://github.com/laravel/passport/issues/917

---
+++ tests/BridgeRefreshTokenRepositoryTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Laravel\Passport\Tests;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Contracts\Events\Dispatcher;
+use Laravel\Passport\Bridge\RefreshTokenRepository;
+
+class BridgeRefreshTokenRepositoryTest extends TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function test_it_can_determine_if_a_refresh_token_is_revoked()
+    {
+        $refreshToken = new RevokedRefreshToken;
+        $repository = $this->repository($refreshToken);
+
+        $this->assertTrue($repository->isRefreshTokenRevoked('tokenId'));
+    }
+
+    public function test_a_refresh_token_is_also_revoked_if_it_cannot_be_found()
+    {
+        $refreshToken = null;
+        $repository = $this->repository($refreshToken);
+
+        $this->assertTrue($repository->isRefreshTokenRevoked('tokenId'));
+    }
+
+    public function test_it_can_determine_if_a_refresh_token_is_not_revoked()
+    {
+        $refreshToken = new ActiveRefreshToken;
+        $repository = $this->repository($refreshToken);
+
+        $this->assertFalse($repository->isRefreshTokenRevoked('tokenId'));
+    }
+
+    private function repository($refreshToken): RefreshTokenRepository
+    {
+        $queryBuilder = m::mock(Builder::class);
+        $queryBuilder->shouldReceive('first')->andReturn($refreshToken);
+        $queryBuilder->shouldReceive('where')->andReturn($queryBuilder);
+
+        $connection = m::mock(Connection::class);
+        $connection->shouldReceive('table')->andReturn($queryBuilder);
+
+        $events = m::mock(Dispatcher::class);
+
+        return new RefreshTokenRepository($connection, $events);
+    }
+}
+
+class ActiveRefreshToken
+{
+    public $revoked = false;
+}
+
+class RevokedRefreshToken
+{
+    public $revoked = true;
+}
