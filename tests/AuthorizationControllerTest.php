9be95a273fdd4624da87dc3873fe5eacb45a6dfa
Matt Allan <mattallan@fastmail.com>
2019-05-21 10:10:16 -0400

    Allow clients to skip the authorization prompt

--- tests/AuthorizationControllerTest.php
+++ tests/AuthorizationControllerTest.php
@@ -3,10 +3,10 @@
 namespace Laravel\Passport\Tests;
 
 use Exception;
-use Laravel\Passport\Client;
 use Mockery as m;
 use Laravel\Passport\Token;
 use Illuminate\Http\Request;
+use Laravel\Passport\Client;
 use Zend\Diactoros\Response;
 use Laravel\Passport\Passport;
 use PHPUnit\Framework\TestCase;
@@ -54,6 +54,8 @@ class AuthorizationControllerTest extends TestCase
 
         $clients = m::mock(ClientRepository::class);
         $clients->shouldReceive('find')->with(1)->andReturn($client = m::mock(Client::class));
+
+        $client->shouldReceive('skipsAuthorization')->andReturn(false);
 
         $response->shouldReceive('view')->once()->andReturnUsing(function ($view, $data) use ($authRequest, $client, $user) {
             $this->assertEquals('passport::authorize', $view);
@@ -133,6 +135,49 @@ class AuthorizationControllerTest extends TestCase
             ->with($user, 'client')
             ->andReturn($token = m::mock(Token::class));
         $token->shouldReceive('getAttribute')->with('scopes')->andReturn(['scope-1']);
+
+        $this->assertEquals('approved', $controller->authorize(
+            m::mock(ServerRequestInterface::class), $request, $clients, $tokens
+        )->getContent());
+    }
+
+    public function test_request_is_approved_if_client_can_skip_authorization()
+    {
+        Passport::tokensCan([
+            'scope-1' => 'description',
+        ]);
+
+        $server = m::mock(AuthorizationServer::class);
+        $response = m::mock(ResponseFactory::class);
+
+        $controller = new AuthorizationController($server, $response);
+        $psrResponse = new Response();
+        $psrResponse->getBody()->write('approved');
+        $server->shouldReceive('validateAuthorizationRequest')
+            ->andReturn($authRequest = m::mock(AuthorizationRequest::class));
+        $server->shouldReceive('completeAuthorizationRequest')
+            ->with($authRequest, m::type(ResponseInterface::class))
+            ->andReturn($psrResponse);
+
+        $request = m::mock(Request::class);
+        $request->shouldReceive('user')->once()->andReturn($user = m::mock());
+        $user->shouldReceive('getKey')->andReturn(1);
+        $request->shouldNotReceive('session');
+
+        $authRequest->shouldReceive('getClient->getIdentifier')->once()->andReturn(1);
+        $authRequest->shouldReceive('getScopes')->once()->andReturn([new Scope('scope-1')]);
+        $authRequest->shouldReceive('setUser')->once()->andReturnNull();
+        $authRequest->shouldReceive('setAuthorizationApproved')->once()->with(true);
+
+        $clients = m::mock(ClientRepository::class);
+        $clients->shouldReceive('find')->with(1)->andReturn($client = m::mock(Client::class));
+
+        $client->shouldReceive('skipsAuthorization')->andReturn(true);
+
+        $tokens = m::mock(TokenRepository::class);
+        $tokens->shouldReceive('findValidToken')
+            ->with($user, $client)
+            ->andReturnNull();
 
         $this->assertEquals('approved', $controller->authorize(
             m::mock(ServerRequestInterface::class), $request, $clients, $tokens
