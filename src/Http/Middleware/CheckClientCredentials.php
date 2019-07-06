9fa0bff6ee7451e4ef8a6b0cf99ce4630819c734
Ali Shafiee <alshf89@gmail.com>
2019-07-06 11:29:51 +0430

    Fix exception will thrown if token belongs to first party clients issue #691

--- src/Http/Middleware/CheckClientCredentials.php
+++ src/Http/Middleware/CheckClientCredentials.php
@@ -5,6 +5,7 @@ namespace Laravel\Passport\Http\Middleware;
 use Closure;
 use Zend\Diactoros\StreamFactory;
 use Zend\Diactoros\ResponseFactory;
+use Laravel\Passport\TokenRepository;
 use Zend\Diactoros\UploadedFileFactory;
 use League\OAuth2\Server\ResourceServer;
 use Zend\Diactoros\ServerRequestFactory;
@@ -23,14 +24,23 @@ class CheckClientCredentials
     protected $server;
 
     /**
+     * Token Repository.
+     *
+     * @var \Laravel\Passport\TokenRepository
+     */
+    protected $repository;
+
+    /**
      * Create a new middleware instance.
      *
      * @param  \League\OAuth2\Server\ResourceServer  $server
+     * @param  \Laravel\Passport\TokenRepository  $repository
      * @return void
      */
-    public function __construct(ResourceServer $server)
+    public function __construct(ResourceServer $server, TokenRepository $repository)
     {
         $this->server = $server;
+        $this->repository = $repository;
     }
 
     /**
@@ -57,27 +67,33 @@ class CheckClientCredentials
             throw new AuthenticationException;
         }
 
-        $this->validateScopes($psr, $scopes);
+        $this->validate($psr, $scopes);
 
         return $next($request);
     }
 
     /**
-     * Validate the scopes on the incoming request.
+     * Validate the scopes and token on the incoming request.
      *
      * @param  \Psr\Http\Message\ServerRequestInterface $psr
      * @param  array  $scopes
      * @return void
-     * @throws \Laravel\Passport\Exceptions\MissingScopeException
+     * @throws \Laravel\Passport\Exceptions\MissingScopeException|\Illuminate\Auth\AuthenticationException
      */
-    protected function validateScopes($psr, $scopes)
+    protected function validate($psr, $scopes)
     {
-        if (in_array('*', $tokenScopes = $psr->getAttribute('oauth_scopes'))) {
+        $token = $this->repository->find($psr->getAttribute('oauth_access_token_id'));
+
+        if (! $token || $token->client->firstParty()) {
+            throw new AuthenticationException;
+        }
+
+        if (in_array('*', $token->scopes)) {
             return;
         }
 
         foreach ($scopes as $scope) {
-            if (! in_array($scope, $tokenScopes)) {
+            if ($token->cant($scope)) {
                 throw new MissingScopeException($scope);
             }
         }
--- src/Http/Middleware/CheckClientCredentialsForAnyScope.php
+++ src/Http/Middleware/CheckClientCredentialsForAnyScope.php
@@ -5,6 +5,7 @@ namespace Laravel\Passport\Http\Middleware;
 use Closure;
 use Zend\Diactoros\StreamFactory;
 use Zend\Diactoros\ResponseFactory;
+use Laravel\Passport\TokenRepository;
 use Zend\Diactoros\UploadedFileFactory;
 use League\OAuth2\Server\ResourceServer;
 use Zend\Diactoros\ServerRequestFactory;
@@ -23,14 +24,23 @@ class CheckClientCredentialsForAnyScope
     private $server;
 
     /**
+     * Token Repository.
+     *
+     * @var \Laravel\Passport\TokenRepository
+     */
+    protected $repository;
+
+    /**
      * Create a new middleware instance.
      *
      * @param  \League\OAuth2\Server\ResourceServer  $server
+     * @param  \Laravel\Passport\TokenRepository  $repository
      * @return void
      */
-    public function __construct(ResourceServer $server)
+    public function __construct(ResourceServer $server, TokenRepository $repository)
     {
         $this->server = $server;
+        $this->repository = $repository;
     }
 
     /**
@@ -57,7 +67,7 @@ class CheckClientCredentialsForAnyScope
             throw new AuthenticationException;
         }
 
-        if ($this->validateScopes($psr, $scopes)) {
+        if ($this->validate($psr, $scopes)) {
             return $next($request);
         }
 
@@ -65,20 +75,27 @@ class CheckClientCredentialsForAnyScope
     }
 
     /**
-     * Validate the scopes on the incoming request.
+     * Validate the scopes and token on the incoming request.
      *
      * @param  \Psr\Http\Message\ServerRequestInterface $psr
      * @param  array  $scopes
      * @return bool
+     * @throws \Illuminate\Auth\AuthenticationException
      */
-    protected function validateScopes($psr, $scopes)
+    protected function validate($psr, $scopes)
     {
-        if (in_array('*', $tokenScopes = $psr->getAttribute('oauth_scopes'))) {
+        $token = $this->repository->find($psr->getAttribute('oauth_access_token_id'));
+
+        if (! $token || $token->client->firstParty()) {
+            throw new AuthenticationException;
+        }
+
+        if (in_array('*', $token->scopes)) {
             return true;
         }
 
         foreach ($scopes as $scope) {
-            if (in_array($scope, $tokenScopes)) {
+            if ($token->can($scope)) {
                 return true;
             }
         }
