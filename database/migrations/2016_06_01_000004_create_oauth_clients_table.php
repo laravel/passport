99ed8fd495c16282b41bd26afd56a3da9e96eb04
Taylor Otwell <taylor@laravel.com>
2019-08-19 09:34:18 -0500

    make column nullable

--- database/migrations/2016_06_01_000004_create_oauth_clients_table.php
+++ database/migrations/2016_06_01_000004_create_oauth_clients_table.php
@@ -17,7 +17,7 @@ class CreateOauthClientsTable extends Migration
             $table->increments('id');
             $table->bigInteger('user_id')->index()->nullable();
             $table->string('name');
-            $table->string('secret', 100);
+            $table->string('secret', 100)->nullable();
             $table->text('redirect');
             $table->boolean('personal_access_client');
             $table->boolean('password_client');
