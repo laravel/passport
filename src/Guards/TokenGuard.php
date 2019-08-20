ee624fc1955df70c1eb3d4a75353e400ef5607dc
Jess Archer <jess@jessarcher.com>
2019-08-20 11:24:29 +1000

    Removes _token input
    Mistakenly copied from VerifyCsrfMiddleware

--- src/Guards/TokenGuard.php
+++ src/Guards/TokenGuard.php
@@ -267,7 +267,7 @@ class TokenGuard
      */
     protected function getTokenFromRequest($request)
     {
-        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
+        $token = $request->header('X-CSRF-TOKEN');
 
         if (! $token && $header = $request->header('X-XSRF-TOKEN')) {
             $token = $this->encrypter->decrypt($header, static::serialized());
