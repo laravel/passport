048c7578cf4fd34237e85898a1282aff898e9440
Taylor Otwell <taylorotwell@gmail.com>
2019-12-15 10:50:19 -0600

    formatting

--- src/Passport.php
+++ src/Passport.php
@@ -423,6 +423,7 @@ class Passport
     public static function actingAsClient($client, $scopes = [])
     {
         $token = app(self::tokenModel());
+
         $token->client = $client;
         $token->scopes = $scopes;
 
