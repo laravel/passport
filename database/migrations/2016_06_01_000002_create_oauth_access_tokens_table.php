63db1d6260a81d971922c7a13cebd2699f002cff
Kieron Wiltshire <KieronWiltshire@users.noreply.github.com>
2020-01-21 15:41:41 +0000

    Update 2016_06_01_000002_create_oauth_access_tokens_table.php

--- database/migrations/2016_06_01_000002_create_oauth_access_tokens_table.php
+++ database/migrations/2016_06_01_000002_create_oauth_access_tokens_table.php
@@ -15,8 +15,9 @@ class CreateOauthAccessTokensTable extends Migration
     {
         Schema::create('oauth_access_tokens', function (Blueprint $table) {
             $table->string('id', 100)->primary();
-            $table->bigInteger('user_id')->index()->nullable();
-            $table->unsignedInteger('client_id');
+            $table->bigInteger('user_id')->unsigned()->nullable()->index();
+            $table->bigInteger('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('oauth_clients')->onDelete('cascade');
             $table->string('name')->nullable();
             $table->text('scopes')->nullable();
             $table->boolean('revoked');
