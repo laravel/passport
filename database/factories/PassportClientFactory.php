d5f7cdc164f55c319a712108473d4e70608d24d2
Antonio Pauletich <antonio.pauletich95@gmail.com>
2020-02-08 22:17:42 +0100

    Add password grant feature tests

--- database/factories/PassportClientFactory.php
+++ database/factories/PassportClientFactory.php
@@ -12,8 +12,15 @@ $factory->define(Client::class, function (Faker $faker) {
         'name' => $faker->company,
         'secret' => Str::random(40),
         'redirect' => $faker->url,
-        'personal_access_client' => 0,
-        'password_client' => 0,
-        'revoked' => 0,
+        'personal_access_client' => false,
+        'password_client' => false,
+        'revoked' => false,
+    ];
+});
+
+$factory->state(Client::class, 'password_client', function (Faker $faker) {
+    return [
+        'personal_access_client' => false,
+        'password_client' => true,
     ];
 });
--- tests/Feature/PassportTestCase.php
+++ tests/Feature/PassportTestCase.php
@@ -3,25 +3,64 @@
 namespace Laravel\Passport\Tests\Feature;
 
 use Illuminate\Contracts\Config\Repository;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
 use Laravel\Passport\PassportServiceProvider;
 use Orchestra\Testbench\TestCase;
 
 abstract class PassportTestCase extends TestCase
 {
+    use DatabaseTransactions;
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->withFactories(__DIR__.'/../../database/factories');
+
+        $this->artisan('migrate:fresh');
+
+        Passport::routes();
 
         $this->artisan('passport:keys');
     }
 
     protected function getEnvironmentSetUp($app)
     {
-        $app->make(Repository::class)->set('auth.guards.api', ['driver' => 'passport', 'provider' => 'users']);
+        $config = $app->make(Repository::class);
+
+        $config->set('auth.defaults.provider', 'users');
+
+        if (null !== ($userClass = $this->getUserClass())) {
+            $config->set('auth.providers.users.model', $userClass);
+        }
+
+        $config->set('auth.guards.api', ['driver' => 'passport', 'provider' => 'users']);
+
+        $config->set('database.default', 'forge');
+
+        $config->set('database.connections.forge', [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'username' => 'root',
+            'password' => '',
+            'database' => 'forge',
+            'prefix' => '',
+        ]);
     }
 
     protected function getPackageProviders($app)
     {
         return [PassportServiceProvider::class];
+    }
+
+    /**
+     * Get the Eloquent user model class name.
+     *
+     * @return string|null
+     */
+    protected function getUserClass(): ?string
+    {
+        return null;
     }
 }
