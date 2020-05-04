d2584334960c95cf2a87bb54a45cbc5f9f0f4fa9
Tom Jamon <contact@tomjamon.com>
2020-05-04 19:02:39 +0200

    Update CheckClientCredentialsForAnyScope.php

--- src/Http/Middleware/CheckClientCredentialsForAnyScope.php
+++ src/Http/Middleware/CheckClientCredentialsForAnyScope.php
@@ -17,7 +17,7 @@ class CheckClientCredentialsForAnyScope extends CheckCredentials
      */
     protected function validateCredentials($token)
     {
-        if (! $token || ! $token->client || $token->client->firstParty()) {
+        if (! $token) {
             throw new AuthenticationException;
         }
     }
