c27d41fee099c96895a9067305b15463e9af0e89
CaliforniaMountainSnake <CaliforniaMountainSnake1@yandex.ru>
2020-05-22 18:45:32 +0300

    The connection name in the "storage" section of the passport config file has been updated to "PASSPORT_DB_CONNECTION".

--- config/passport.php
+++ config/passport.php
@@ -59,7 +59,7 @@ return [
 
     'storage' => [
         'database' => [
-            'connection' => env('DB_CONNECTION', 'mysql'),
+            'connection' => env('PASSPORT_DB_CONNECTION', env('DB_CONNECTION', 'mysql')),
         ],
     ],
 
