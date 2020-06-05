c242d48085ad59663e6d3a7302cb0fabfbbc79b3
Dries Vints <dries@vints.io>
2020-06-05 15:38:22 +0200

    Apply fixes from StyleCI (#1291)

--- tests/Feature/PassportTestCase.php
+++ tests/Feature/PassportTestCase.php
@@ -12,9 +12,9 @@ abstract class PassportTestCase extends TestCase
 {
     use RefreshDatabase;
 
-    const KEYS = __DIR__ . '/keys';
-    const PUBLIC_KEY = self::KEYS . '/oauth-public.key';
-    const PRIVATE_KEY = self::KEYS . '/oauth-private.key';
+    const KEYS = __DIR__.'/keys';
+    const PUBLIC_KEY = self::KEYS.'/oauth-public.key';
+    const PRIVATE_KEY = self::KEYS.'/oauth-private.key';
 
     protected function setUp(): void
     {
