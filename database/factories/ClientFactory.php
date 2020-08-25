0c98fcadc0ac5783458a2ac1f9ec98c7bc4f1788
Dries Vints <dries@vints.io>
2020-08-25 19:20:38 +0200

    Support Laravel 8

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Laravel\Passport\Database\Factories;
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Laravel\Passport\Client;
+
+class ClientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Client::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => null,
+            'name' => $this->faker->company,
+            'secret' => Str::random(40),
+            'redirect' => $this->faker->url,
+            'personal_access_client' => false,
+            'password_client' => false,
+            'revoked' => false,
+        ];
+    }
+
+    /**
+     * Use as Password Client.
+     *
+     * @return $this
+     */
+    public function asPasswordClient()
+    {
+        return $this->state([
+            'personal_access_client' => false,
+            'password_client' => true,
+        ]);
+    }
+}
