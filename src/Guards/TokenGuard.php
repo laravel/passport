53d6516d5bc0988bf231730e756558eef386676d
Leandro Grando <leandro-grando@hotmail.com>
2020-10-09 10:16:50 -0300

    Avoid decrypt exception if Mac is invalid

--- src/Guards/TokenGuard.php
+++ src/Guards/TokenGuard.php
@@ -279,7 +279,11 @@ class TokenGuard
         $token = $request->header('X-CSRF-TOKEN');
 
         if (! $token && $header = $request->header('X-XSRF-TOKEN')) {
-            $token = $this->encrypter->decrypt($header, static::serialized());
+            try {
+                $token = $this->encrypter->decrypt($header, static::serialized());
+            } catch (Exception $e) {
+                return;
+            }
         }
 
         return $token;
