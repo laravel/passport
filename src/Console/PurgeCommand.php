985577cec359d57df15ac641bfaed84fdc222824
Alexander Karlstad <alexander.karlstad@gmail.com>
2021-01-14 15:11:49 +0100

    [10.x] Add purging of invalid refresh tokens to command (#1396)
    * Delete invalid refresh tokens
    
    Delete refresh tokens referring to non-existing access tokens.
    
    * Use whereDoesntHave() instead
    
    * Add test for purge command
    
    * Update description and output of command
    
    * Update PurgeCommand.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Console/PurgeCommand.php
+++ src/Console/PurgeCommand.php
@@ -22,7 +22,7 @@ class PurgeCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Purge revoked and / or expired tokens and authentication codes';
+    protected $description = 'Purge revoked and / or expired tokens, authentication codes, and refresh tokens.';
 
     /**
      * Execute the console command.
@@ -36,20 +36,23 @@ class PurgeCommand extends Command
             Passport::token()->where('revoked', 1)->orWhereDate('expires_at', '<', $expired)->delete();
             Passport::authCode()->where('revoked', 1)->orWhereDate('expires_at', '<', $expired)->delete();
             Passport::refreshToken()->where('revoked', 1)->orWhereDate('expires_at', '<', $expired)->delete();
+            Passport::refreshToken()->whereDoesntHave('accessToken')->delete();
 
-            $this->info('Purged revoked items and items expired for more than seven days.');
+            $this->info('Purged invalid refresh tokens, revoked tokens, and tokens expired for more than seven days.');
         } elseif ($this->option('revoked')) {
             Passport::token()->where('revoked', 1)->delete();
             Passport::authCode()->where('revoked', 1)->delete();
             Passport::refreshToken()->where('revoked', 1)->delete();
+            Passport::refreshToken()->whereDoesntHave('accessToken')->delete();
 
-            $this->info('Purged revoked items.');
+            $this->info('Purged invalid refresh tokens and revoked tokens.');
         } elseif ($this->option('expired')) {
             Passport::token()->whereDate('expires_at', '<', $expired)->delete();
             Passport::authCode()->whereDate('expires_at', '<', $expired)->delete();
             Passport::refreshToken()->whereDate('expires_at', '<', $expired)->delete();
+            Passport::refreshToken()->whereDoesntHave('accessToken')->delete();
 
-            $this->info('Purged items expired for more than seven days.');
+            $this->info('Purged invalid refresh tokens and tokens expired for more than seven days.');
         }
     }
 }
