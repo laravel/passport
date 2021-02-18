b28cd974cbdca374c15a8ba6cd3533c2719e2447
Dries Vints <dries@vints.io>
2021-02-18 16:06:38 +0100

    Update to phpseclib v3 (#1410)

--- src/Console/KeysCommand.php
+++ src/Console/KeysCommand.php
@@ -3,9 +3,8 @@
 namespace Laravel\Passport\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Arr;
 use Laravel\Passport\Passport;
-use phpseclib\Crypt\RSA;
+use phpseclib3\Crypt\RSA;
 
 class KeysCommand extends Command
 {
@@ -28,10 +27,9 @@ class KeysCommand extends Command
     /**
      * Execute the console command.
      *
-     * @param  \phpseclib\Crypt\RSA  $rsa
      * @return void
      */
-    public function handle(RSA $rsa)
+    public function handle()
     {
         [$publicKey, $privateKey] = [
             Passport::keyPath('oauth-public.key'),
@@ -41,10 +39,10 @@ class KeysCommand extends Command
         if ((file_exists($publicKey) || file_exists($privateKey)) && ! $this->option('force')) {
             $this->error('Encryption keys already exist. Use the --force option to overwrite them.');
         } else {
-            $keys = $rsa->createKey($this->input ? (int) $this->option('length') : 4096);
+            $key = RSA::createKey($this->input ? (int) $this->option('length') : 4096);
 
-            file_put_contents($publicKey, Arr::get($keys, 'publickey'));
-            file_put_contents($privateKey, Arr::get($keys, 'privatekey'));
+            file_put_contents($publicKey, (string) $key->getPublicKey());
+            file_put_contents($privateKey, (string) $key);
 
             $this->info('Encryption keys generated successfully.');
         }
