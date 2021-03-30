04d5d5fdcccbf49577d9625ec587ed2a6db7eed8
AgonAziri <agonn.azirii@gmail.com>
2021-03-30 10:33:23 +0200

    Changes to pass continuous-integration/styleci/pr

--- src/Guards/TokenGuard.php
+++ src/Guards/TokenGuard.php
@@ -124,8 +124,8 @@ class TokenGuard
     public function client(Request $request, $psr = null)
     {
         if ($request->bearerToken()) {
-            if(! $psr){
-                if(! $psr = $this->getPsrRequestViaBearerToken($request)){
+            if(! $psr) {
+                if(! $psr = $this->getPsrRequestViaBearerToken($request)) {
                     return;
                 }
             }
@@ -153,6 +153,7 @@ class TokenGuard
         }
 
         $client = $this->hasValidProvider($request, $psr);
+
         
         if (! $client) {
             return;
