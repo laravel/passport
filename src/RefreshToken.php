95a2258b396af786e5a8ae2a5e9ed3fa7c1a57ce
Sergey Pashkevich <siarheipashkevich@gmail.com>
2023-03-17 16:39:15 +0300

    Update RefreshToken.php (#1645)

--- src/RefreshToken.php
+++ src/RefreshToken.php
@@ -41,15 +41,7 @@ class RefreshToken extends Model
      */
     protected $casts = [
         'revoked' => 'bool',
-    ];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'expires_at',
+        'expires_at' => 'datetime',
     ];
 
     /**
