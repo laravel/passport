a1682d39b7730d318d1a6e3139836e06bda692d4
Sergey Pashkevich <siarheipashkevich@gmail.com>
2023-03-17 16:39:26 +0300

    Update AuthCode.php (#1644)

--- src/AuthCode.php
+++ src/AuthCode.php
@@ -34,15 +34,7 @@ class AuthCode extends Model
      */
     protected $casts = [
         'revoked' => 'bool',
-    ];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'expires_at',
+        'expires_at' => 'datetime',
     ];
 
     /**
