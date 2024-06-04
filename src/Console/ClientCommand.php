48cf94810144909c835a487b5787b92116ecd173
Taylor Otwell <taylor@laravel.com>
2024-06-04 08:39:59 -0500

    Update ClientCommand.php

--- src/Console/ClientCommand.php
+++ src/Console/ClientCommand.php
@@ -71,7 +71,7 @@ class ClientCommand extends Command
         $this->components->info('Personal access client created successfully.');
 
         if (! config('passport.personal_access_client')) {
-            $this->components->info('You should set `PASSPORT_PERSONAL_ACCESS_CLIENT_ID` and `PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET` environment variables.');
+            $this->components->info('Please define the `PASSPORT_PERSONAL_ACCESS_CLIENT_ID` and `PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET` environment variables.');
         }
 
         $this->outputClientDetails($client);
