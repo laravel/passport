423a4ec69337303a91ec372fa0681a9156c77dc0
Taylor Otwell <taylor@laravel.com>
2024-06-24 22:40:29 +0200

    Update 2016_06_01_000001_create_oauth_auth_codes_table.php

--- database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
+++ database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('oauth_auth_codes', function (Blueprint $table) {
             $table->char('id', 80)->primary();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->index();
             $table->foreignId('client_id');
             $table->text('scopes')->nullable();
             $table->boolean('revoked');
