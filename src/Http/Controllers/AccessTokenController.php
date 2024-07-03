51eba554418f3837c71e9ede0e5b20010c9339dd
Hafez Divandari <hafezdivandari@gmail.com>
2024-07-03 18:17:25 +0330

    [13.x] disable PAT request

--- src/Http/Controllers/AccessTokenController.php
+++ src/Http/Controllers/AccessTokenController.php
@@ -4,6 +4,7 @@ namespace Laravel\Passport\Http\Controllers;
 
 use Laravel\Passport\TokenRepository;
 use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
 use Nyholm\Psr7\Response as Psr7Response;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -44,10 +45,17 @@ class AccessTokenController
      *
      * @param  \Psr\Http\Message\ServerRequestInterface  $request
      * @return \Illuminate\Http\Response
+     *
+     * @throws \Laravel\Passport\Exceptions\OAuthServerException
      */
     public function issueToken(ServerRequestInterface $request)
     {
         return $this->withErrorHandling(function () use ($request) {
+            if (array_key_exists('grant_type', $attributes = (array) $request->getParsedBody())
+                && $attributes['grant_type'] === 'personal_access') {
+                throw OAuthServerException::unsupportedGrantType();
+            }
+
             return $this->convertResponse(
                 $this->server->respondToAccessTokenRequest($request, new Psr7Response)
             );
