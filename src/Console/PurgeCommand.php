b6cdb30d68edbd52b07535ccd3fc968a1e2259ef
Hafez Divandari <hafezdivandari@gmail.com>
2024-07-09 20:24:45 +0330

    formatting

--- src/Console/PurgeCommand.php
+++ src/Console/PurgeCommand.php
@@ -39,33 +39,18 @@ class PurgeCommand extends Command
             : false;
 
         Passport::token()->where(function (Builder $query) use ($revoked, $expired) {
-            $query->when($revoked, function ($query) {
-                $query->orWhere('revoked', true);
-            });
-
-            $query->when($expired, function ($query, $expired) {
-                $query->orWhere('expires_at', '<', $expired);
-            });
+            $query->when($revoked, fn (Builder $query) => $query->orWhere('revoked', true));
+            $query->when($expired, fn (Builder $query) => $query->orWhere('expires_at', '<', $expired));
         })->delete();
 
         Passport::authCode()->where(function (Builder $query) use ($revoked, $expired) {
-            $query->when($revoked, function ($query) {
-                $query->orWhere('revoked', true);
-            });
-
-            $query->when($expired, function ($query, $expired) {
-                $query->orWhere('expires_at', '<', $expired);
-            });
+            $query->when($revoked, fn (Builder $query) => $query->orWhere('revoked', true));
+            $query->when($expired, fn (Builder $query) => $query->orWhere('expires_at', '<', $expired));
         })->delete();
 
         Passport::refreshToken()->where(function (Builder $query) use ($revoked, $expired) {
-            $query->when($revoked, function ($query) {
-                $query->orWhere('revoked', true);
-            });
-
-            $query->when($expired, function ($query, $expired) {
-                $query->orWhere('expires_at', '<', $expired);
-            });
+            $query->when($revoked, fn (Builder $query) => $query->orWhere('revoked', true));
+            $query->when($expired, fn (Builder $query) => $query->orWhere('expires_at', '<', $expired));
         })->delete();
 
         $this->components->info(sprintf('Purged %s.', implode(' and ', array_filter([
