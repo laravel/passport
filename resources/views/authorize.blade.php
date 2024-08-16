24a646473017e38d9554b26857ee01aa677a6919
Hafez Divandari <hafezdivandari@gmail.com>
2024-08-16 17:19:53 +0330

    revert irrelevant changes

--- resources/views/authorize.blade.php
+++ resources/views/authorize.blade.php
@@ -38,56 +38,56 @@
     </style>
 </head>
 <body class="passport-authorize">
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card card-default">
-                <div class="card-header">
-                    Authorization Request
-                </div>
-                <div class="card-body">
-                    <!-- Introduction -->
-                    <p><strong>{{ $client->name }}</strong> is requesting permission to access your account.</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Authorization Request
+                    </div>
+                    <div class="card-body">
+                        <!-- Introduction -->
+                        <p><strong>{{ $client->name }}</strong> is requesting permission to access your account.</p>
 
-                    <!-- Scope List -->
-                    @if (count($scopes) > 0)
-                        <div class="scopes">
-                            <p><strong>This application will be able to:</strong></p>
+                        <!-- Scope List -->
+                        @if (count($scopes) > 0)
+                            <div class="scopes">
+                                    <p><strong>This application will be able to:</strong></p>
 
-                            <ul>
-                                @foreach ($scopes as $scope)
-                                    <li>{{ $scope->description }}</li>
-                                @endforeach
-                            </ul>
+                                    <ul>
+                                        @foreach ($scopes as $scope)
+                                            <li>{{ $scope->description }}</li>
+                                        @endforeach
+                                    </ul>
+                            </div>
+                        @endif
+
+                        <div class="buttons">
+                            <!-- Authorize Button -->
+                            <form method="post" action="{{ route('passport.authorizations.approve') }}">
+                                @csrf
+
+                                <input type="hidden" name="state" value="{{ $request->state }}">
+                                <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
+                                <input type="hidden" name="auth_token" value="{{ $authToken }}">
+                                <button type="submit" class="btn btn-success btn-approve">Authorize</button>
+                            </form>
+
+                            <!-- Cancel Button -->
+                            <form method="post" action="{{ route('passport.authorizations.deny') }}">
+                                @csrf
+                                @method('DELETE')
+
+                                <input type="hidden" name="state" value="{{ $request->state }}">
+                                <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
+                                <input type="hidden" name="auth_token" value="{{ $authToken }}">
+                                <button class="btn btn-danger">Cancel</button>
+                            </form>
                         </div>
-                    @endif
-
-                    <div class="buttons">
-                        <!-- Authorize Button -->
-                        <form method="post" action="{{ route('passport.authorizations.approve') }}">
-                            @csrf
-
-                            <input type="hidden" name="state" value="{{ $request->state }}">
-                            <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
-                            <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                            <button type="submit" class="btn btn-success btn-approve">Authorize</button>
-                        </form>
-
-                        <!-- Cancel Button -->
-                        <form method="post" action="{{ route('passport.authorizations.deny') }}">
-                            @csrf
-                            @method('DELETE')
-
-                            <input type="hidden" name="state" value="{{ $request->state }}">
-                            <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
-                            <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                            <button class="btn btn-danger">Cancel</button>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 </html>
