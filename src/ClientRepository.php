811a15ed5eefe4a292636574c8a25bd875a1f59f
Hafez Divandari <hafezdivandari@gmail.com>
2024-08-26 03:21:41 +0330

    fix client repo

--- src/ClientRepository.php
+++ src/ClientRepository.php
@@ -151,9 +151,9 @@ class ClientRepository
     /**
      * Store a new password grant client.
      */
-    public function createPasswordGrantClient(string $name, ?string $provider = null): Client
+    public function createPasswordGrantClient(string $name, ?string $provider = null, bool $confidential = false): Client
     {
-        return $this->create($name, ['password', 'refresh_token'], [], $provider);
+        return $this->create($name, ['password', 'refresh_token'], [], $provider, $confidential);
     }
 
     /**
@@ -171,7 +171,7 @@ class ClientRepository
      */
     public function createImplicitGrantClient(string $name, array $redirectUris): Client
     {
-        return $this->create($name, ['implicit'], $redirectUris);
+        return $this->create($name, ['implicit'], $redirectUris, null, false);
     }
 
     /**
