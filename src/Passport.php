7a4c0a16f83805919e53e0ca7536f571785c42f0
Owen Voke <development@voke.dev>
2025-05-02 13:05:52 +0100

    fix: resolve access to uninitialised typed static property

--- src/Passport.php
+++ src/Passport.php
@@ -82,7 +82,7 @@ class Passport
     /**
      * The storage location of the encryption keys.
      */
-    public static string $keyPath;
+    public static string|null $keyPath = null;
 
     /**
      * The access token entity class name.
