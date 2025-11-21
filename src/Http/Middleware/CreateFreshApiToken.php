32b063797239ce4a7f3e7a234ce1f8e91f59ee57
Hafez Divandari <hafezdivandari@gmail.com>
2025-11-21 18:03:20 +0330

    fix minor cs

--- src/Http/Middleware/CreateFreshApiToken.php
+++ src/Http/Middleware/CreateFreshApiToken.php
@@ -38,7 +38,7 @@ class CreateFreshApiToken
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  (\Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response))  $next
      */
     public function handle(Request $request, Closure $next, ?string $guard = null): BaseResponse
     {
