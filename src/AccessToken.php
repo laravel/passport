4a19cf6cb0cdf2bfd6076b8e75e3928a2eb44181
Frank Korf <fjkorf@gmail.com>
2026-02-10 12:36:24 -0600

    Add mixin annotation for `Token` model in `AccessToken`

--- src/AccessToken.php
+++ src/AccessToken.php
@@ -13,6 +13,8 @@ use Psr\Http\Message\ServerRequestInterface;
  * @template TValue
  *
  * @implements \Illuminate\Contracts\Support\Arrayable<string, TValue>
+ * 
+ * @mixin \Laravel\Passport\Token
  *
  * @property string $oauth_access_token_id
  * @property string $oauth_client_id
